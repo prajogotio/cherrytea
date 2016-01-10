3ef93cb7c36e12951faf7a635c8794bddf74700b
prajogotio <ggkgaya@hotmail.com>
2016-01-10 06:33:00 +0000

    cloudinary update_project integration

--- static/js/update_project.js
+++ static/js/update_project.js
@@ -1,6 +1,7 @@
 (function($){
 	var values = {};
 	$(document).ready(function(){
+		registerUploadPhotoCloudinary(values, 'proj_pic');
 		$('#proj-pic-file').change(function(){
 			if (this.files.length > 0){
 				var f = this.files[0];
@@ -54,4 +55,22 @@
 			});
 		});
 	});
+	function registerUploadPhotoCloudinary(values, pic_id) {
+		$('.form-upload').append($.cloudinary.unsigned_upload_tag("i9hka0um",  { cloud_name: 'ddkd4u5ja' }));
+		$('.form-upload').unsigned_cloudinary_upload("i9hka0um", 
+  				{ cloud_name: 'ddkd4u5ja'}
+		).bind('cloudinarydone', function(e, data) {
+			$('#form-upload-info').html('Uploaded!');
+			$.ajax({
+				url:'http://'+location.host+'/store/pic',
+				method:'POST',
+				data:{'pic_url':data.result.secure_url}
+			}).done(function(msg) {
+				values[pic_id] = msg.pic_id;
+			});
+		}).bind('cloudinaryprogress', function(e, data) { 
+		  $('#form-upload-info').html('Uploading: ' +  
+		    Math.round((data.loaded * 100.0) / data.total) + '%'); 
+		});;
+	}
 })(jQuery)
